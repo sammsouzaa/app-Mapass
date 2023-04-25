e826d5ca5cf990298d8a3d8a126b75e6b8a971e1
samuel souza <124898734+sammsouzaa@users.noreply.github.com>
2023-04-25 16:59:27 -0300

    Update Main_Activity.java

--- Main_Activity.java
+++ Main_Activity.java
@@ -1,21 +1,34 @@
-package com.example.novo;
+package com.example.appmapas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText IDnomeestado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        IDnomeestado = findViewById(R.id.IDnomeestado);
+
     }
     public void telaMapas(View view){
 
         Intent it_telaMapas = new Intent(this, telaMapas.class);
+
         startActivity(it_telaMapas);
 
         //setContentView(R.layout.activity_main);
