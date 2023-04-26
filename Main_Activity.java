0927beba16b170a9b477f5eb4ebfaabdf9152ecb
samuel souza <124898734+sammsouzaa@users.noreply.github.com>
2023-04-26 14:55:05 -0300

    Update Main_Activity.java

--- Main_Activity.java
+++ Main_Activity.java
@@ -2,30 +2,72 @@ package com.example.appmapas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    public EditText IDnomeestado;
+    public TextInputLayout nomeestado;
+
+    //@SuppressLint("MissingInflatedId")
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nomeestado = findViewById(R.id.IDestado);
+
+    }
+    public boolean validatenome(){
+
+        String estado = nomeestado.getEditText().getText().toString().trim();
+
+        if (estado.isEmpty()){
+            nomeestado.setError("Field can't be empty!!");
+            return false;
+        }else{
+            nomeestado.setError(null);
+            nomeestado.setErrorEnabled(false);
+            return true;
+        }
+
     }
     public void telamapas(View view){
 
+        if (!validatenome()) {
+            return;
+        }
+
         Intent it_telamapas = new Intent(this, telamapas.class);
+
+        if (nomeestado.equals("pr")|| nomeestado.equals("PR")){
+
+            it_telamapas.putExtra("nomeestado", "parana");
+            it_telamapas.putExtra("siglaestado", "pr");
+        }
+
+        else if (nomeestado.equals("sc")|| nomeestado.equals("SC")){
+
+            it_telamapas.putExtra("nomeestado", "santa catarina");
+            it_telamapas.putExtra("siglaestado", "sc");
+        }
+
+        else if (nomeestado.equals("rs")|| nomeestado.equals("RS")){
+
+            it_telamapas.putExtra("nomeestado", "rio grande do sul");
+            it_telamapas.putExtra("siglaestado", "rs");
+        }
 
         startActivity(it_telamapas);
 
-        //setContentView(R.layout.activity_main);
     }
 }
